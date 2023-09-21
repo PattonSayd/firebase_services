6ed1eb15faf805da3f7b602f4a638ceeb2a78aeb
NsGun <pattonsayd@gmail.com>
2023-09-21 18:12:41 +0400

    flutterfire configure

--- lib/app/di.dart
+++ lib/app/di.dart
@@ -1,9 +1,26 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_services/app/logger.dart';
+import 'package:firebase_services/firebase_options.dart';
+import 'package:flutter/material.dart';
 import 'package:get_it/get_it.dart';
 
 abstract class Locator {
   static final _locator = GetIt.instance;
 
-  static Future<void> init() async {}
+  static Future<void> init() async {
+    WidgetsFlutterBinding.ensureInitialized();
+    await _initFirebase();
+  }
+
+  static Future<void> _initFirebase() async {
+    logger.d('Firebase initialization started');
+
+    await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform,
+    );
+
+    logger.d('Firebase initialized');
+  }
 
   static Future<void> dispose() async {}
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:firebase_services/app/app.dart';
+import 'package:firebase_services/app/di.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+Future<void> main() async {
+  await Locator.init();
   runApp(const App());
 }
